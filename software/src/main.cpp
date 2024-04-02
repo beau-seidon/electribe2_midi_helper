d03b9b2ef095019cc0aaf48950292580ba96747f
Beau Sterling <beau.seidon@outlook.com>
2024-04-02 00:59:04 -0500

    clean up code and fix gitignore

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,15 +1,8 @@
 #include <Arduino.h>
 #include <MIDI.h>
 
-// This example shows how to create two instances of the library to create a merger.
-// There are two MIDI couples of IO, A and B, each using thru and merging with the
-// input from the other node. The result is the following:
-// A out = A in + B in
-// B out = B in + A in
 
 
-// #include <SoftwareSerial.h>
-// SoftwareSerial softSerial(2,3);
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1,  midiA);
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial2,  midiB);
 
@@ -19,31 +12,27 @@ int blink_delay = 20;  // [ms]
 
 
 void setup() {
-    // Initiate MIDI communications, listen to all channels
     midiA.begin(MIDI_CHANNEL_OMNI);
     midiB.begin(MIDI_CHANNEL_OMNI);
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
+
 void loop() {
     if (midiA.read()) {
-        // Thru on A has already pushed the input message to out A.
-        // Forward the message to out B as well.
         midiB.send(midiA.getType(),
-                   midiA.getData1(),
-                   midiA.getData2(),
-                   midiA.getChannel());
+                    midiA.getData1(),
+                    midiA.getData2(),
+                    midiA.getChannel());
         digitalWrite(LED_BUILTIN, HIGH);
         blink_time = millis();
     }
 
     if (midiB.read()) {
-        // Thru on B has already pushed the input message to out B.
-        // Forward the message to out A as well.
         midiA.send(midiB.getType(),
-                   midiB.getData1(),
-                   midiB.getData2(),
-                   midiB.getChannel());
+                    midiB.getData1(),
+                    midiB.getData2(),
+                    midiB.getChannel());
         digitalWrite(LED_BUILTIN, HIGH);
         blink_time = millis();
     }
